199c94ad8606682870199734bb0470284d7a0cb3
inhyeok <ss112d86@gmail.com>
2023-07-22 15:28:07 +0900

    feat: 아키텍처 초기 셋팅

---
+++ src/main/java/com/movte/slate/domain/auth/application/usecase/AuthUseCase.java
@@ -0,0 +1,14 @@
+package com.movte.slate.domain.auth.application.usecase;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthUseCase {
+
+    public void auth() {
+    }
+
+}
