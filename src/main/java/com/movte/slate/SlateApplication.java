8bac3fdb18a1cd10470912d34d73cb5c34a746f2
inhyeok <ss112d86@gmail.com>
2023-07-22 15:20:59 +0900

    chore: 프로젝트 초기 구성

---
+++ src/main/java/com/movte/slate/SlateApplication.java
@@ -0,0 +1,13 @@
+package com.movte.slate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movte/slate/SlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movte.slate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
