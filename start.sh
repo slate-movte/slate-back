9717b7838d2db01649ca6e95974ff5887e94872c
KBS <qudtjs0753@naver.com>
2023-08-26 17:05:43 +0900

    feat(#18): application.yml에 설정 동시에 해줄 수 있도록 yml 형식으로 env 수정
    Signed-off-by: KBS <qudtjs0753@naver.com>

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 cd docker_compose_local
-docker compose down
-docker compose --env-file ../.env build --no-cache
-docker compose --env-file ../.env up -d
+docker compose --env-file ../.env.yml down
+docker compose --env-file ../.env.yml build --no-cache
+docker compose --env-file ../.env.yml up -d
---
+++ stop.sh
@@ -0,0 +1,2 @@
+cd docker_compose_local
+docker compose --env-file ../.env.yml down
