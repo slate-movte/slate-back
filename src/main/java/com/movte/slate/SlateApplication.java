9c14e43d37907250cf4a46c814e838aa093c788e
조성찬 <wordnumperiod@gmail.com>
2023-08-29 19:43:35 +0900

    feat: Property Scan을 위한 어노테이션 추가
    - Redis, Jwt 속성 값을 application.yml에서 읽어오기 위해 필요한 어노테이션이다.
    
    Refs: #20

--- src/main/java/com/movte/slate/SlateApplication.java
+++ src/main/java/com/movte/slate/SlateApplication.java
@@ -2,8 +2,10 @@ package com.movte.slate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SlateApplication {
 
     public static void main(String[] args) {
