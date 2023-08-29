fdf9120f2cc498c9f341b4899bd82a0efe4b3e17
조성찬 <wordnumperiod@gmail.com>
2023-08-29 09:29:41 +0900

    Feat: Web Security 설정 추가
    - CORS 설정 및 JWT 인증 설정
    
    Refs: #14

---
+++ src/main/java/com/movte/slate/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.movte.slate.config;
+
+import com.movte.slate.oidc.JwtAuthenticationFilter;
+import com.movte.slate.oidc.JwtExceptionHandlerFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+
+@Configuration
+@EnableWebSecurity // 현재 클래스를 스프링 필터체인에 등록
+@Log4j2
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtExceptionHandlerFilter jwtExceptionHandlerFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    // 인가가 필요한 리소스 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeRequests(requests -> {
+                    requests.requestMatchers(new AntPathRequestMatcher("/api/login")).permitAll(); // 로그인 경로는 모든 사용자에게 허락
+                    requests.requestMatchers(new AntPathRequestMatcher("/api")).authenticated(); // 그 외에는 인증된 사용자만 허락
+                })
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 쓸 때 사용
+                )
+                .addFilterBefore(jwtExceptionHandlerFilter, UsernamePasswordAuthenticationFilter.class) // ExceptionHandler 필터가 앞에 와야 함!
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // UsernamePasswordAuthenticationFilter 앞에 JwtFilter 추가
+        ;
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // cross-origin 요청이 허가되는 url
+        configuration.setAllowedOrigins(List.of("*"));
+        //허용할 헤더 설정
+        configuration.addAllowedHeader("*");
+        //허용할 http method
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
