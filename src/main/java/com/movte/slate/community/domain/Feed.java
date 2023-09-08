76ec6925553080dcf8c9560ce6d3a2e82babbee7
seongchan cho <wordnumperiod@gmail.com>
2023-09-08 19:37:42 +0900

    feat: 커뮤니티 관련 엔티티 클래스에 BaseTimeEntity 적용
    - ERD를 보면, Comment, Feed, Follow, Like에 등록 시간 컬럼이 있는 것을 확인할 수 있음.
    - 이를 BaseEntity로 구현함
    
    Refs: #70

--- src/main/java/com/movte/slate/community/domain/Feed.java
+++ src/main/java/com/movte/slate/community/domain/Feed.java
@@ -1,11 +1,12 @@
 package com.movte.slate.community.domain;
 
+import com.movte.slate.domain.common.BaseTimeEntity;
 import com.movte.slate.domain.user.domain.User;
 
 import javax.persistence.*;
 
 @Entity
-public class Feed {
+public class Feed extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
